97449306bac0b7c618456befb9043bc84ce82622
Juan David Pacheco <juandapv995@gmail.com>
2026-01-21 21:46:51 -0500

    Add required_providers block to Terraform config
    Introduced a terraform block specifying required providers for aws and archive, including their sources and versions. Also updated the provider block to use lowercase 'aws' for consistency.

--- aws/main.tf
+++ aws/main.tf
@@ -1,4 +1,17 @@
-provider "AWS" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive" 
+      version = "~> 2.4.2"
+    }
+  }
+}
+
+provider "aws" {
     region = var.region
     #profile = "default"
     default_tags {
